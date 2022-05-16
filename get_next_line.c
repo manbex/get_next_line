0f62003f2810017b366743b05611601264c25c9b
manbex <105050536+manbex@users.noreply.github.com>
2022-05-16 23:02:34 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -5,54 +5,163 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbenicho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/10 22:21:56 by mbenicho          #+#    #+#             */
-/*   Updated: 2022/05/10 22:21:57 by mbenicho         ###   ########.fr       */
+/*   Created: 2022/05/16 17:32:20 by mbenicho          #+#    #+#             */
+/*   Updated: 2022/05/16 17:32:21 by mbenicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-char	*ft_read(int fd, char *buf, char *str)
+size_t	ft_strlen(char *s)
 {
-	char	last;
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	size;
 	int		i;
 
-	last = ' ';
-	while (last != '\n')
+	i = -1;
+	if (!s1)
 	{
-		i = 0;
-		while (last != '\n' && i < BUFFER_SIZE)
-		{
-			if (read(fd, &buf[i], 1) <= 0)
-				return (NULL);
-			last = buf[i];
-			i++;
-		}
-		buf[i] = 0;
-		str = ft_strdup_cat(str, buf);
-		if (!str)
+		s1 = malloc(sizeof(char));
+		if (!s1)
 			return (NULL);
+		*s1 = 0;
 	}
+	size = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc((size + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	while (s1[++i])
+		str[i] = s1[i];
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = 0;
+	return (free(s1), str);
+}
+
+int		ft_have_newline(char *stash)
+{
+	size_t	i;
+
+	i = 0;
+	if (stash)
+	{
+		while (stash[i] && stash[i] != '\n')
+			i++;
+		if (stash[i] == '\n')	
+			return (1);
+	}
+	return (0);
+}
+
+char	*get_line(char *stash)
+{
+	size_t	i;
+	size_t	size;
+	char	*str;
+
+	size = 0;
+	if (!stash || !stash[0])
+		return (NULL);
+	while (stash[size] && stash[size] != '\n')
+		size++;
+	str = malloc((size + 2) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i <= size)
+	{
+		str[i] = stash[i];
+		i++;
+	}
+	str[i] = 0;
 	return (str);
+}
+
+char	*get_stash(char *stash, int readed)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	while (*stash && *stash != '\n')
+		stash++;
+	if (!*stash && readed)
+		return (free(stash), NULL);
+	while (stash[i])
+		i++;
+	str = malloc((i + 1) * sizeof(char));
+	if (!str)
+		return (free(stash), NULL);
+	i = 0;
+	while (*stash)
+		str[i++] = *stash++;
+	str[i] = 0;
+	return (free(stash), str);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*buf;
-	char	*str;
+	char		*buf;
+	static char	*stash;
+	int			readed;
+	char		*str;
 
-	if (BUFFER_SIZE <= 0 || fd < 0 || fd > 1024)
+	if (BUFFER_SIZE < 1)
 		return (NULL);
 	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf)
 		return (NULL);
-	str = malloc(1);
-	if (!str)
-		return (free(buf), NULL);
-	str[0] = 0;
-	str = ft_read(fd, buf, str);
-	free(buf);
-	if (!str)
-		return (NULL);
-	return (str);
+	readed = 1;
+	while (!ft_have_newline(stash) && readed)
+	{
+		readed = read(fd, buf, BUFFER_SIZE);
+		if (readed < 0)
+			return (free(buf), NULL);
+		buf[readed] = 0;
+		stash = ft_strjoin(stash, buf);
+	}
+	str = get_line(stash);
+	stash = get_stash(stash, readed);
+	return (free(buf), str);
+}
+
+
+#include <fcntl.h>
+int	main()
+{
+	int		fd;
+	char	*str;
+
+	fd = open("txt", O_RDONLY);
+
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+
+	/*str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);*/
+
+	close(fd);
+	return (0);
 }
